a3fc0fac77d1d853ff6933c31404d4781857464c
Михаил Мыслицкий <mysmster@yandex.ru>
2023-05-14 15:17:26 +0300

    add situation_tags table

--- app/core/base.py
+++ app/core/base.py
@@ -1,4 +1,4 @@
 # app/core/base.py
 """Импорты класса Base и всех моделей для Alembic."""
 from app.core.db import Base  # noqa
-from app.models.duellio import Situation, Tags  # noqa
+from app.models.duellio import Situation, Tags, situation_tags  # noqa
--- app/models/duellio.py
+++ app/models/duellio.py
@@ -1,20 +1,29 @@
 # app/models/duellio.py
 
 # Добавьте импорт класса Text.
-from sqlalchemy import Column, String, Text
+from sqlalchemy import Column, String, Text, Table, ForeignKey
 from sqlalchemy.orm import relationship
 
 from app.core.db import Base
+
+situation_tags = Table(
+    "situations_tags",
+    Base.metadata,
+    Column("situation_id", ForeignKey("situation.id"), primary_key=True),
+    Column("tag_id", ForeignKey("tags.id"), primary_key=True),
+)
 
 
 class Situation(Base):
     title = Column(String(250), unique=True, nullable=False)
     text = Column(Text, nullable=False)
-    tags = relationship("Situation", secondary="situations_tags", back_populates="situations")
+    tags = relationship(
+        "Situation", secondary="situation_tags", back_populates="situations"
+    )
 
 
 class Tags(Base):
     name = Column(String(250), unique=True, nullable=False)
-    situations = relationship("Tag", secondary="situations_tags", back_populates="tags")
-
-
+    situations = relationship(
+        "Tag", secondary="situation_tags", back_populates="tags"
+    )
