04bd713a03b2081b48afe03bb46e351fbcf6ce7a
Михаил Мыслицкий <mysmster@yandex.ru>
2023-06-25 20:42:22 +0300

    add CORS permissions

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 # app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_pagination import add_pagination
 
 from app.api.routers import main_router
 from app.core.config import settings
 
 app = FastAPI(title=settings.app_title, description=settings.description)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(main_router)
 
